16e50c67ee8d8c0ab35e3126c57044c3a9fb2bf2
mihinsumaria <mihin.sumaria@gmail.com>
2017-10-29 15:53:17 -0400

    Added Food model (with category), created superuser

--- restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/models.py
+++ restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/models.py
@@ -3,3 +3,14 @@ from django.utils import timezone
 
 
 # Create your models here.
+class Food(models.Model):
+	ID=models.BigIntegerField(primary_key=True)
+	name=models.CharField(max_length=50)
+	description=models.CharField(max_length=300)
+	price=models.FloatField()
+	course=models.CharField(max_length=50)
+	cuisine=models.CharField(max_length=50)
+	category=models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name
