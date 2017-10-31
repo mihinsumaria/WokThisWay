84871516e6987238dce50366508aa2efbfab085d
Vaseem Shaikh <vaseem@gmail.com>
2017-10-31 15:12:03 -0400

    - Added models for Manager, Cashier, Customer, Order - Added __str__() for each class

--- restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/models.py
+++ restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/models.py
@@ -11,3 +11,34 @@ class Food(models.Model):
 
 	def __str__(self):
 		return self.name
+
+class Customer(models.Model):
+    ID =  models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=300)
+    def __str__(self):
+        return self.name
+
+class Cashier(models.Model):
+    ID =  models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class Manager(models.Model):
+    ID =  models.BigIntegerField(primary_key=True)
+    name= models.CharField(max_length=50)
+    password = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    ID = models.AutoField(primary_key = True)
+    table_id= models.IntegerField()
+    status = models.CharField(max_length = 100)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT,)
+    food = models.ManyToManyField(Food, verbose_name="Food items", )
