9097f284c7042be8a1f7406f2f928d77a178190b
Vaseem Shaikh <vaseem@gmail.com>
2017-10-31 12:52:14 -0400

    - Added demo homepage - Added urls.py in the app - added templates folder in app - Added index.html in templates

--- restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/models.py
+++ restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/models.py
@@ -1,12 +1,4 @@
 from django.db import models
-class Food(models.Model):
-    course= models.CharField(max_length = 50)
-    cuisine= models.CharField(max_length = 50)
-    name= models.CharField(max_length = 50)
-    description= models.CharField(max_length = 250)
-    price= models.FloatField()
-    food_id = models.BigIntegerField(primary_key=True)
-
 # Create your models here.
 class Food(models.Model):
 	ID=models.BigIntegerField(primary_key=True)
@@ -19,6 +11,3 @@ class Food(models.Model):
 
 	def __str__(self):
 		return self.name
-
-
-
