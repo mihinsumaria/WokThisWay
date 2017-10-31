0aa49661a58003cf446737910b74b124cc4be3b0
Vaseem Shaikh <vaseem@gmail.com>
2017-10-31 15:27:41 -0400

    - Registered Models to admin

--- restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/admin.py
+++ restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Food
+from .models import Food, Cashier, Manager, Customer, Order
 
 admin.site.register(Food)
+admin.site.register(Cashier)
+admin.site.register(Manager)
+admin.site.register(Order)
+admin.site.register(Customer)
+
 
 # Register your models here.
