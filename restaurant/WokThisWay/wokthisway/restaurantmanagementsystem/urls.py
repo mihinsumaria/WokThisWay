4f6ee015e0cd526d37de11c451d3defd4ec2076a
Vaseem Shaikh <vaseem@gmail.com>
2017-10-31 20:51:25 -0400

    - Added views for 	- Chinese food 	- American Food 	- Dessert - Removed extra html files 	- only one html for all food lists - Improved file structure

--- restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/urls.py
+++ restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
 
     #locahost/menu/beverage
     url(r'^beverage/$',views.beverage_menu, name = 'beverage'),
-    #locahost/menu/indian_food
+    #locahost/menu/indian_menu
     url(r'^indian_menu/$',views.indian_menu, name = 'indian_menu'),
+    #locahost/menu/chinese_menu
+    url(r'^chinese_menu/$',views.chinese_menu, name = 'chinese_menu'),
+    #locahost/menu/american_menu
+    url(r'^american_menu/$',views.american_menu, name = 'american_menu'),
+    #locahost/menu/dessert
+    url(r'^dessert/$',views.dessert, name = 'dessert'),
 ]
--- restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/views.py
+++ restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/views.py
@@ -7,9 +7,21 @@ def index(request):
     return render(request,'restaurantmanagementsystem/index.html',)
 
 def beverage_menu(request):
-    beverage_list = Food.objects.filter(cuisine = 'Beverage')
-    return render(request,'restaurantmanagementsystem/beverage.html',{'beverage_list':beverage_list})
+    food_list = Food.objects.filter(cuisine = 'Beverage')
+    return render(request,'restaurantmanagementsystem/food_menu.html',{'food_list':food_list})
 
 def indian_menu(request):
-    indain_food_list = Food.objects.filter(cuisine = 'Indian')
-    return render(request,'restaurantmanagementsystem/indian_food.html',{'indain_food_list':indain_food_list})
+    food_list = Food.objects.filter(cuisine = 'Indian')
+    return render(request,'restaurantmanagementsystem/food_menu.html',{'food_list':food_list})
+
+def chinese_menu(request):
+    food_list = Food.objects.filter(cuisine = 'Chinese')
+    return render(request,'restaurantmanagementsystem/food_menu.html',{'food_list':food_list})
+
+def american_menu(request):
+    food_list = Food.objects.filter(cuisine = 'American')
+    return render(request,'restaurantmanagementsystem/food_menu.html',{'food_list':food_list})
+
+def dessert(request):
+    food_list = Food.objects.filter(cuisine = 'Dessert')
+    return render(request,'restaurantmanagementsystem/food_menu.html',{'food_list':food_list})
