ba58a1719aa457e45d861e3a196ac2e9344f2d22
Vaseem Shaikh <vaseem@gmail.com>
2017-10-31 20:34:03 -0400

    - Added html files for indian food and beverages menu - Added functions for respectuve menu in views

--- restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/urls.py
+++ restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+
+    #localhost/
     url(r'^$', views.index, name= 'index' ),
+
+    #locahost/menu/beverage
+    url(r'^beverage/$',views.beverage_menu, name = 'beverage'),
+    #locahost/menu/indian_food
+    url(r'^indian_menu/$',views.indian_menu, name = 'indian_menu'),
 ]
--- restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/views.py
+++ restaurant/WokThisWay/wokthisway/restaurantmanagementsystem/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import Food
 
 # Create your views here.
 
 def index(request):
     return render(request,'restaurantmanagementsystem/index.html',)
+
+def beverage_menu(request):
+    beverage_list = Food.objects.filter(cuisine = 'Beverage')
+    return render(request,'restaurantmanagementsystem/beverage.html',{'beverage_list':beverage_list})
+
+def indian_menu(request):
+    indain_food_list = Food.objects.filter(cuisine = 'Indian')
+    return render(request,'restaurantmanagementsystem/indian_food.html',{'indain_food_list':indain_food_list})
